60eb0cec4ebdeac394412e771d717b5da650b65c
Александр <lisyanscky.sasha2567@yandex.ru>
2016-11-16 08:07:21 +0300

    Немного разного

---
+++ Лабы/МОП/lab1/src/main/scala/lab5.scala
@@ -0,0 +1,14 @@
+package main.scala
+
+/**
+  * Created by user on 13.11.2016.
+  */
+object lab5 {
+  def десятичноеЧислоВNичное(c: Int, p: Int) : Int = {
+    c
+  }
+
+  def main(args: Array[String]): Unit = {
+
+  }
+}

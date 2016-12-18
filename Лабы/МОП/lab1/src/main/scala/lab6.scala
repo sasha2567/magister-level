d788100d528f21e7386a67cb7e8d840370522739
Александр <lisyanscky.sasha2567@yandex.ru>
2016-12-19 02:21:43 +0300

    6 МОП + 6 ТПИС

--- Лабы/МОП/lab1/src/main/scala/lab6.scala
+++ Лабы/МОП/lab1/src/main/scala/lab6.scala
@@ -4,24 +4,24 @@ package main.scala
   * Created by user on 23.11.2016.
   */
 object lab6 {
-  class Box[T] {
-    var value : T
+  case class Box[T](var value : T) {
+    def save(v : T) = {value = v}
+    def get: T = value
+  }
 
-    def save(v : T) : T = {
-      value = v
-    }
-
-    def get() : T = {
-      value
-    }
+  case class MyPrinter[T](attribute: T) {
+    def print()           = println(attribute)
+    def print2(arg: T)    = println(attribute + " and " + arg)
+    def print3[R](arg: R) = println(attribute + " and " + arg)
   }
 
   class VBox[T] {
     def identity[T](x: T) = x
-
   }
 
   def main(args: Array[String]): Unit = {
-
+    val box : Box[Int] = Box(2)
+    box.save(25)
+    println(box.get)
   }
 }

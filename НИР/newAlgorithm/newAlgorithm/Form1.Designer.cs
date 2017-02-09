9554bf0869823fea894515ca868fee0c9452f77c
sasha2567 <lisyanscky.sasha2567@yandex.ru>
2017-02-09 09:29:25 +0300

    Изменение в форме

--- НИР/newAlgorithm/newAlgorithm/Form1.Designer.cs
+++ НИР/newAlgorithm/newAlgorithm/Form1.Designer.cs
@@ -40,12 +40,24 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(12, 25);
+            this.numericUpDown1.Location = new System.Drawing.Point(15, 33);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             5,
             0,
@@ -60,14 +72,14 @@
             this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
             this.numericUpDown1.TabIndex = 0;
             this.numericUpDown1.Value = new decimal(new int[] {
-            1,
+            3,
             0,
             0,
             0});
             // 
             // countBatchesTB
             // 
-            this.countBatchesTB.Location = new System.Drawing.Point(12, 65);
+            this.countBatchesTB.Location = new System.Drawing.Point(15, 73);
             this.countBatchesTB.Name = "countBatchesTB";
             this.countBatchesTB.Size = new System.Drawing.Size(100, 20);
             this.countBatchesTB.TabIndex = 1;
@@ -75,7 +87,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 180);
+            this.button1.Location = new System.Drawing.Point(15, 188);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(136, 23);
             this.button1.TabIndex = 2;
@@ -86,7 +98,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 6);
+            this.label1.Location = new System.Drawing.Point(16, 14);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(138, 13);
             this.label1.TabIndex = 3;
@@ -95,7 +107,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 46);
+            this.label2.Location = new System.Drawing.Point(16, 54);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(178, 13);
             this.label2.TabIndex = 4;
@@ -103,7 +115,7 @@
             // 
             // timeSwitchingTB
             // 
-            this.timeSwitchingTB.Location = new System.Drawing.Point(12, 111);
+            this.timeSwitchingTB.Location = new System.Drawing.Point(15, 119);
             this.timeSwitchingTB.Name = "timeSwitchingTB";
             this.timeSwitchingTB.Size = new System.Drawing.Size(100, 20);
             this.timeSwitchingTB.TabIndex = 5;
@@ -111,7 +123,7 @@
             // 
             // timeTreatmentingTB
             // 
-            this.timeTreatmentingTB.Location = new System.Drawing.Point(12, 154);
+            this.timeTreatmentingTB.Location = new System.Drawing.Point(15, 162);
             this.timeTreatmentingTB.Name = "timeTreatmentingTB";
             this.timeTreatmentingTB.Size = new System.Drawing.Size(100, 20);
             this.timeTreatmentingTB.TabIndex = 6;
@@ -119,7 +131,7 @@
             // 
             // LTB
             // 
-            this.LTB.Location = new System.Drawing.Point(158, 111);
+            this.LTB.Location = new System.Drawing.Point(161, 119);
             this.LTB.Name = "LTB";
             this.LTB.Size = new System.Drawing.Size(100, 20);
             this.LTB.TabIndex = 7;
@@ -128,7 +140,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 92);
+            this.label3.Location = new System.Drawing.Point(15, 100);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(138, 13);
             this.label3.TabIndex = 8;
@@ -137,7 +149,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 138);
+            this.label4.Location = new System.Drawing.Point(16, 146);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(125, 13);
             this.label4.TabIndex = 9;
@@ -146,7 +158,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(157, 92);
+            this.label5.Location = new System.Drawing.Point(160, 100);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(97, 13);
             this.label5.TabIndex = 10;
@@ -154,7 +166,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(160, 180);
+            this.button2.Location = new System.Drawing.Point(163, 188);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(94, 38);
             this.button2.TabIndex = 11;
@@ -162,28 +174,105 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(615, 372);
+            this.tabControl1.TabIndex = 12;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.numericUpDown1);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.countBatchesTB);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.LTB);
+            this.tabPage1.Controls.Add(this.timeSwitchingTB);
+            this.tabPage1.Controls.Add(this.timeTreatmentingTB);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(607, 346);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Установка параметров";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(607, 346);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Установка времени";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(317, 16);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(282, 24);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Время переналадки приборов";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(321, 43);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(278, 288);
+            this.dataGridView2.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(4, 16);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(280, 24);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Время обработки требований";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(4, 43);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(280, 288);
+            this.dataGridView1.TabIndex = 17;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.LTB);
-            this.Controls.Add(this.timeTreatmentingTB);
-            this.Controls.Add(this.timeSwitchingTB);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.countBatchesTB);
-            this.Controls.Add(this.numericUpDown1);
+            this.ClientSize = new System.Drawing.Size(636, 396);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -201,6 +290,13 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- НИР/newAlgorithm/newAlgorithm/Form1.cs
+++ НИР/newAlgorithm/newAlgorithm/Form1.cs
@@ -12,9 +12,34 @@ namespace newAlgorithm
 {
     public partial class Form1 : Form
     {
+        int l, maxS, maxT;
+        int countType, countBatches;
+        List<List<List<int>>> temptS = new List<List<List<int>>>();
+        List<List<int>> temptT = new List<List<int>>();
+
         public Form1()
         {
             InitializeComponent();
+            InitializeForm();
+        }
+
+        private void InitializeForm()
+        {
+            countType = (int)numericUpDown1.Value;
+            countBatches = Convert.ToInt32(countBatchesTB.Text);
+            List<int> listCountButches = new List<int>();
+            for (int ii = 0; ii < countType; ii++)
+            {
+                listCountButches.Add(countBatches);
+            }
+
+            l = Convert.ToInt32(LTB.Text);
+            maxS = Convert.ToInt32(timeSwitchingTB.Text);
+            maxT = Convert.ToInt32(timeTreatmentingTB.Text);
+            temptS = new List<List<List<int>>>();
+            temptT = new List<List<int>>();
+            RandomTime();
+            PrintTime();
         }
 
         private List<int> copy(List<int> _in)
@@ -27,7 +52,8 @@ namespace newAlgorithm
             return result;
         }
 
-        private List<List<int>> составыПартий(List<List<int>> _in){
+        private List<List<int>> составыПартий(List<List<int>> _in)
+        {
             List<List<int>> A = new List<List<int>>();
             int count = 0;
             A.Add(new List<int>());
@@ -45,27 +71,14 @@ namespace newAlgorithm
                         A[count][j]++;
                         count++;
                     }
-                    while (A[count - 1][0] > A[count - 1][j]);       
+                    while (A[count - 1][0] > A[count - 1][j]);
                 }
             }
             return A;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void RandomTime()
         {
-            int countType = (int)numericUpDown1.Value;
-            int countBatches = Convert.ToInt32(countBatchesTB.Text);
-            List<int> listCountButches = new List<int>();
-            for (int ii = 0; ii < countType; ii++)
-            {
-                listCountButches.Add(countBatches);
-            }
-            int l, maxS, maxT;
-            l = Convert.ToInt32(LTB.Text);
-            maxS = Convert.ToInt32(timeSwitchingTB.Text);
-            maxT = Convert.ToInt32(timeTreatmentingTB.Text);
-            List<List<List<int>>> temptS = new List<List<List<int>>>();
-            List<List<int>> temptT = new List<List<int>>();
             int count = 0;
             for (int i = 0; i < l; i++)
             {
@@ -84,6 +97,66 @@ namespace newAlgorithm
                     count++;
                 }
             }
+        }
+
+        private void PrintTime()
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            dataGridView1.RowCount = temptT.Count;
+            dataGridView1.ColumnCount = temptT[0].Count;
+            for (int i = 0; i < temptT.Count; i++)
+            {
+                for (int j = 0; j < countType; j++)
+                {
+                    dataGridView1[j, i].Value = temptT[i][j];
+                }
+            }
+            dataGridView2.Rows.Clear();
+            dataGridView2.Columns.Clear();
+            dataGridView2.RowCount = temptS.Count * temptS[0].Count;
+            dataGridView2.ColumnCount = temptS[0].Count;
+            for (int k = 0; k < temptS.Count; k++)
+                for (int i = 0; i < countType; i++)
+                    for (int j = 0; j < countType; j++)
+                    {
+                        int str = k * temptS[0].Count + i;
+                        dataGridView2[j, str].Value = temptS[k][i][j];
+                    }
+        }
+
+        private void GetTime()
+        {
+            for (int i = 0; i < temptT.Count; i++)
+            {
+                for (int j = 0; j < countType; j++)
+                {
+                    temptT[i][j] = Convert.ToInt32(dataGridView1[j, i].Value);
+                }
+            }
+            for (int k = 0; k < temptS.Count; k++)
+                for (int i = 0; i < countType; i++)
+                    for (int j = 0; j < countType; j++)
+                    {
+                        int str = k * temptS[0].Count + i;
+                        temptS[k][i][j] = Convert.ToInt32(dataGridView2[j, str].Value);
+                    }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            countType = (int)numericUpDown1.Value;
+            countBatches = Convert.ToInt32(countBatchesTB.Text);
+            List<int> listCountButches = new List<int>();
+            for (int ii = 0; ii < countType; ii++)
+            {
+                listCountButches.Add(countBatches);
+            }
+
+            l = Convert.ToInt32(LTB.Text);
+            maxS = Convert.ToInt32(timeSwitchingTB.Text);
+            maxT = Convert.ToInt32(timeTreatmentingTB.Text);
+            GetTime();            
             Shedule.L = l;
             Shedule.maxTimeSwitching = maxS;
             Shedule.maxTimeTreatment = maxT;
@@ -91,7 +164,7 @@ namespace newAlgorithm
             Shedule.TTreatment = temptT;
             FirstLevel firstLevel = new FirstLevel(countType, listCountButches);
             firstLevel.GenetateSolutionForAllTypes();
-            
+
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -101,7 +174,7 @@ namespace newAlgorithm
             var S = gaa.ToArray();
             var shedule = new Shedule(S);
             shedule.ConstructShedule();
-            var s=shedule.GetTime();
+            var s = shedule.GetTime();
 
         }
     }
